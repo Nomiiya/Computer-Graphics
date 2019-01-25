7a0e0da8d9091e1751e56d61b3a918646f5d605a
Nomiiya <46464936+Nomiiya@users.noreply.github.com>
2019-01-25 11:02:15 -0700

    Basic

---
+++ Trial/WebGLFundamentals.js
@@ -0,0 +1,11 @@
+/*
+ Web trial for webGL via: https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
+
+ Mark Buenaflor
+*/
+
+var canvas = index.getElementById("c");
+var gl = canvas.getContext("web");
+if(!gl){
+
+}
