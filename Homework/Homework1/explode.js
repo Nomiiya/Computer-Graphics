4e918e7c4b361a1f524e3c088692d41bc362244d
Nomiiya <46464936+Nomiiya@users.noreply.github.com>
2019-02-13 10:12:05 -0700

    HW2
    Homework2 added
    Homework1 Updated

--- Homework/Homework1/explode.js
+++ Homework/Homework1/explode.js
@@ -6,7 +6,13 @@ Written by: HW470:Mark Terence Buenaflor
 Date: Jan 2019
 
 Description:
-This program ..... HW470: COMPLETE THIS. DESCRIBE WHAT YOU DID.
+This program explodes from the middle in a sort of "hallway style look" using size illusions.
+I kept adding thinner and thinner triangles to add to the illusion.
+
+HW470:
+I added to the demo that was given to us since it looked like the explosed could be formed in a
+sort of hall way looking illusion if done properly.
+
 */
 
 var canvas;
@@ -15,19 +21,21 @@ var gl;
 //store the vertices
 //Each triplet represents one triangle
 var vertices = [];
-var vert2 = [];
-
 //store a color for each vertex
 var colors = [];
 
 //HW470: control the explosion
 //(Your variables here)
-var theta = 0.0;
-var thetaLoc;
-var numY = 0.25;
+var variation = 0.0;
+var variationLoc;
+
+var colorVariation = 0.1;
+var colorVariationLoc;
+
+var frame = 0;
+
 //HW470: control the redraw rate
-var delay = 20;
-var direction = true;
+var delay = 60;
 
 // =============== function init ======================
 
@@ -52,25 +60,54 @@ window.onload = function init()
 	// happy with my initial design. (Just an idea for you; no need to use.)
 	//(During the explosion all geometry must remain in the window.)
 	var sval = 0.25;
+  var val2 = 0.3;
     vertices = [
         vec2( sval, -sval ),// core
         vec2( 0.0, sval ),
         vec2( -sval, -sval ),
-        vec2(-.4, -.4),
-        vec2(0.0, 0.4),
-        vec2(-0.4, 0.4),
-        vec2(.4, .4),
-        vec2(0.0, -0.4),
-        vec2(0.4, -0.4),
-    ];
+        // Left Triangle (Yellow)
+        vec2(-val2, val2),
+        vec2(0, val2),
+        vec2(-val2, -val2),
+        // Right Triangle (Blue)
+        vec2(val2, val2),
+        vec2(0, val2),
+        vec2(val2, -val2),
+        // Lower Triagle ( Dark Blue)
+        vec2(-val2, -val2),
+        vec2(0, -.4),
+        vec2(val2, -val2),
+        // Outter Explosions
+        vec2(-val2, val2),
+        vec2(0, val2),
+        vec2(-val2, -val2),
+        // Right Outter
+        vec2(val2, val2),
+        vec2(0, val2),
+        vec2(val2, -val2),
+        //inner "walls"
+        vec2(-0.26, 0.26),
+        vec2(0, 0.26),
+        vec2(-0.26, -0.26),
 
-    var sval2 = 0.7;
-    vert2 = [
-      vec2( -sval2, -sval2),
-      vec2( 0.0, sval2),
-      vec2( -sval2, sval2)
-    ];
+        vec2(0.26, 0.26),
+        vec2(0, 0.26),
+        vec2(0.26, -0.26),
 
+        // Lower Triagle ( Dark Blue)
+        vec2(-sval, -sval),
+        vec2(0, -.3),
+        vec2(sval, -sval),
+        // Lower Triagle ( Dark Blue)
+        vec2(-val2, -val2),
+        vec2(0, -.4),
+        vec2(val2, -val2),
+
+        // Lower Triagle ( Dark Blue)
+        vec2(-.35, -.35),
+        vec2(0, -.4),
+        vec2(.35, -.35)
+    ];
 
 	//HW470: Create colors for the core and outer parts
 	// See HW specs for the number of colors needed
@@ -84,12 +121,34 @@ window.onload = function init()
     colors.push(vec3(0.0, 1.0, 1.0));
     colors.push(vec3(0.0, 1.0, 1.0));
     colors.push(vec3(0.0, 1.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(1.0, 0.0, 1.0));
+    colors.push(vec3(0.0, 1.0, 0.0));
+    colors.push(vec3(0.0, 1.0, 0.0));
+    colors.push(vec3(0.0, 1.0, 0.0));
+    colors.push(vec3(0.5, 0.8, 0.8));
+    colors.push(vec3(0.5, 0.8, 0.8));
+    colors.push(vec3(0.5, 0.8, 0.8));
 	};
 
 	// HW470: Print the input vertices and colors to the console
-	console.log("Input vertices and colors:");
-
-
+	console.log("Input vertices and colors: Color goes through the four colors for outer to white, vertices 4-30 are for the outer explosion.");
 
     //  Configure WebGL
     gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -115,19 +174,18 @@ window.onload = function init()
     var vbuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vbuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-    //gl.bufferData( gl.ARRAY_BUFFER, flatten(vert2), gl.STATIC_DRAW );
     // Associate shader variables for x,y vertices
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
 	  //HW470: associate shader explode variable ("Loc" variables defined here)
-    thetaLoc = gl.getUniformLocation( program, "theta" );
+    variationLoc = gl.getUniformLocation( program, "variation" );
+    colorVariationLoc = gl.getUniformLocation(program, "colorVariation");
 
     console.log("Data loaded to GPU -- Now call render");
 
     render();
-
 };
 
 
@@ -135,106 +193,31 @@ window.onload = function init()
 
 function render()
 {
-    // clear the screen
-    gl.clear( gl.COLOR_BUFFER_BIT );
+  // clear the screen
+  gl.clear( gl.COLOR_BUFFER_BIT );
 
 
 	//HW470: send uniform(s) to vertex shader
-  console.log(vertices[0], vertices[1],vertices[2]);
-
-  numY += 0.5 ;
-  vertices[0] = vec2(numY, -numY);
-  vertices[1] = vec2(0.0 , numY);
-  vertices[2] = vec2(-numY, -numY);
-
   //HW470: draw the object
 	// You will need to change this to create the exploding outer parts effect
 	// Hint: you will need more than one draw function call
-  gl.drawArrays( gl.TRIANGLES, 0, vertices.length );
-  //gl.drawArrays( gl.TRIANGLES, 0, vert2.length );
+
+  // First inner needs to stay the same SIZE AND COLOR
+  gl.uniform1f(variationLoc, 1);
+  gl.uniform1f(colorVariationLoc, 0);
+  gl.drawArrays( gl.TRIANGLES, 0, 3 );
+
+  // OUTER needs to change COLOR AND SIZE
+  gl.uniform1f(variationLoc, frame * 0.25);
+  gl.uniform1f(colorVariationLoc, frame * 0.05);
+  gl.drawArrays( gl.TRIANGLES, 3, 30 );
+
+  // Associate shader variables for x,y vertices
+  colorVariation += 0.025;
+  gl.uniform1f(colorVariationLoc, colorVariation);
+  frame++;
 
 
 	//re-render after delay
-//  for(i = 0; i < 10; i++){
-	   setTimeout(function (){requestAnimFrame(render);}, delay);
-//  }
+	setTimeout(function (){requestAnimFrame(render);}, delay);
 }
-/*
-function drawTriangle(){
-    // Make the vertices
-    sval = 0.3;
-    var vertices = [
-      vec2( sval, -sval ),// core
-      vec2( .5, sval ),
-      vec2( -sval, -sval )
-    ];
-
-    //Color it
-    for(var i=0; i < vertices.length; i++) {
-  		colors.push(vec3(1.0, 0.0, 0.0));
-      colors.push(vec3(1.0, 0.0, 0.0));
-      colors.push(vec3(1.0, 0.0, 0.0));
-  	};
-
-    // color buffer: create, bind, and load
-      var cBuffer = gl.createBuffer();
-      gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-      gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
-
-    // Associate shader variable for  r,g,b color
-    var vColor = gl.getAttribLocation( program, "vColor" );
-      gl.vertexAttribPointer( vColor, 3, gl.FLOAT, false, 0, 0 );
-      gl.enableVertexAttribArray( vColor );
-
-      // vertex buffer: create, bind, load
-      var vbuffer = gl.createBuffer();
-      gl.bindBuffer( gl.ARRAY_BUFFER, vbuffer );
-      gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-
-      // Associate shader variables for x,y vertices
-      var vPosition = gl.getAttribLocation( program, "vPosition" );
-      gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-      gl.enableVertexAttribArray( vPosition );
-
-      render();
-}
-
-/*
-// General Format for drawing a new Triangle
-function drawTriangle(){
-  var n = initTriangleBuffer();
-  //error check see if we have enough vertices
-  if( n < 0){
-    console.log('Error: Could not get positions');
-    return;
-  }
-  gl.drawArrays(gl.TRIANGLES, 0, n);
-}
-
-// General Initialization of a Triangle
-function initTriangleBuffer() {
-  var vertices = new Float32Array([
-    +0.5, -0.5, 0.0, 0.0, +0.5, 0.0
-  ]);
-  var n = 3;
-
-  var vertexBuffer = gl.createBuffer();
-  if(!vertexBuffer){
-    console.log('Failed to create the buffer object');
-    return -1;
-  }
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-  var aPosition = gl.getAttribLocation(program, 'aPosition');
-  if(aPosition < 0){
-    console.log('Failed to get the storage location of aPosition');
-    return -1;
-  }
-
-  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(aPosition);
-  return n;
-}
-*/
